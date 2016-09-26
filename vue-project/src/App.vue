f0b887e25dadd040ca5ca4da3bb1a2fa0596e526
Mrxiaoxi <18216491949@163.com>
2016-09-26 23:46:06 +0800

    vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,37 +1,69 @@
 <template>
   <div id="app">
     <h2>{{msg}}</h2>
+    <input type="text" v-model="newItem" v-on:keyup.enter="addNew">
     <h3 v-text="msg"></h3>
     <ul>
       <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinished(item)">
         {{item.work}}
       </li>
     </ul>
+    <p>{{newItem}}</p>
+
+    <p>{{message}}</p>
+    <button v-on:click="reverseMessage">Resverse Message</button>
+    <hr />
+    <!-- 综合 todo list  -->
+    <input v-model="newTodo" type="text" v-on:keyup.enter="addTodo">
+    <ul>
+      <li v-for="todo in todos">
+        <span>{{todo.text}}</span>
+        <button v-on:click="removeTodo($index)">del</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
 
 export default {
   data (){
     return {
       msg:"第一个vue.js",
-      items:[
-        {
-          work:"coding",
-          isFinished:false
-        },
-        {
-          work:"eating",
-          isFinished:true
-        }
-      ],
+      items:[],
+      newItem:"",
+      message:"hello Vue",
+      newTodo:"",
+      todos:[
+        {text:"第一个todolist"}
+      ]
     }
+
   },
   methods:{
     toggleFinished:function(item){
       return item.isFinished = !item.isFinished;
+    },
+    addNew:function(){
+      this.items.push({
+        work:this.newItem,
+        isFinished:false
+      });
+      this.newItem = "";
+    },
+    reverseMessage:function(){
+      this.message = this.message.split("").reverse().join("");
+    },
+    addTodo:function(){
+      var text = this.newTodo.trim();
+      if(text){
+        this.todos.push({text:text});
+        this.newTodo="";
+      }
+    },
+    removeTodo:function(index){
+      this.todos.splice(index,1)
     }
   }
 }
